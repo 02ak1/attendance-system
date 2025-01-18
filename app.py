9e968327ba4197cf95e638fd06e664541b93bb17
Y. Ozaki <ozkytjt126@gmail.com>
2025-01-18 13:53:50 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+from check_isct import check_schedule
+
+
+# タイトルの設定
+st.title("勤怠管理アプリ")
+
+# ファイルアップロード機能
+work_file = st.file_uploader("科学大の業務報告ファイルをアップロードしてください", type=["xlsx"])
+schedule_file = st.file_uploader("記入済みのスケジュールファイルをアップロードしてください", type=["xlsx"])
+
+file_path = "schedule.xlsx"
+# ファイルを読み込んでバイナリデータを取得
+with open(file_path, "rb") as file:
+    file_data = file.read()
+# ダウンロードボタンを表示
+st.download_button(
+    label="スケジュールファイルをダウンロードはこちらから",
+    data=file_data,
+    file_name="schedule.xlsx",
+    mime="application/vnd.ms-excel"
+)
+if st.button("確認する"):
+    if work_file and schedule_file:
+        with pd.ExcelFile(work_file) as work_data, pd.ExcelFile(schedule_file) as schedule_data:
+            work_df= work_data.parse(work_data.sheet_names[0],header=None)
+            schedule_df = schedule_data.parse(schedule_data.sheet_names[0])
+            check_schedule(work_df, schedule_df)
+    else:
+        st.warning("両方のファイルをアップロードしてください")    
+        #st.write(work_df)
+        
+    # # アップロードされたエクセルファイルを読み込む
+    # _data = pd.ExcelFile(work_file)
+    
+    # # シート名を選択
+    # sheet_name = st.selectbox("解析するシートを選択してください", excel_data.sheet_names)
+    
+    # # 選択したシートをデータフレームとして読み込む
+    # df = _data.parse(sheet_name,header=None)
+    
+    # データを表示
+    #st.subheader("データプレビュー")
+    #st.write(df)
+    
+    
---
+++ check_isct.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import streamlit as st
+def check_schedule(work_df,schedule_df):
+    has_error = False
+    if pd.isnull(work_df.iloc[1][11]):
+        st.write('業務時間表に名前が入力されていません')
+        return
+    name=work_df.iloc[1][11] #名前の取得
+    name = name.replace('　', '').replace(' ', '')
+    for i in range(6,len(work_df)):
+        day_of_week = work_df.iloc[i][2]
+        for j in range(4,17,4):
+            if pd.isnull(work_df.iloc[i][j]):
+                continue
+            start_time = work_df.iloc[i][j]
+            end_time = work_df.iloc[i][j+1]
+            for k in range(len(schedule_df)):
+                schedule_df['Name'][k]=schedule_df['Name'][k].replace('　', '').replace(' ', '')
+                if name != schedule_df['Name'][k]:
+                    continue
+                if day_of_week == schedule_df['Day_of_Week'][k]:
+                    if start_time<=schedule_df['End_Time'][k] and end_time>=schedule_df['Start_Time'][k]:
+                        has_error = True
+                        st.warning(f'{work_df.iloc[i][1].date()}({work_df.iloc[i][2]})の{schedule_df["Start_Time"][k]}から{schedule_df["End_Time"][k]}は{schedule_df["Name"][k]}さんが{schedule_df["Description"][k]}に参加中であるため勤務できません。')
+    if has_error == False:
+        st.write("報告された勤務時間は問題ありません")
+
+with pd.ExcelFile('work.xlsx') as work_data, pd.ExcelFile('schedule.xlsx') as schedule_data:
+    
+    # ファイル操作
+    work_df= work_data.parse(work_data.sheet_names[0],header=None)
+    schedule_df = schedule_data.parse(schedule_data.sheet_names[0])
+    #print(work_df[:][2])
+    
+    # for i in range(6,len(work_df)):
+    #     day_of_week = work_df.iloc[i][2]
+    #     for j in range(4,17,4):
+    #         if pd.isnull(work_df.iloc[i][j]):
+    #             continue
+    #         start_time = work_df.iloc[i][j]
+    #         end_time = work_df.iloc[i][j+1]
+    #         for k in range(len(schedule_df)):
+    #             if day_of_week == schedule_df['Day_of_Week'][k]:
+    #                 if start_time<=schedule_df['End_Time'][k] and end_time>=schedule_df['Start_Time'][k]:
+    #                     print(f'エラー:{work_df.iloc[i][1].date()}({work_df.iloc[i][2]})の{schedule_df["Start_Time"][k]}から{schedule_df["End_Time"][k]}は{schedule_df["Name"][k]}さんが{schedule_df["Description"][k]}中のため勤務できません。')
+                        
+    check_schedule(work_df,schedule_df)
